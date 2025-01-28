4b1330bf16a9c0d7503de1f1df7dcedd4a16e9a5
AronJustoC <aronchoque727@gmail.com>
2025-01-27 20:26:10 -0500

    fix: resolve conflicts to connect React app do API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,24 @@
 import ListHeader from "./components/ListHeader"
+import { useEffect } from "react"
 
 const App = () => {
 
-  const signOut = () => {
-    console.log('signing out')
-  }
+  const getData = async (req, res) => {
+    const userEmail = 'arito@test.com';
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`);
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error);
+    };
+  };
+
+  useEffect(() => getData, []);
 
   return (
     <div className="app">
       <ListHeader listName={'🏝️ Holiday tick list'} />
-      <div className="button-container">
-        <button type="create">ADD NEW</button>
-        <button type="signout" onClick={signOut}>SIGN OUT</button>
-      </div>
     </div>
   )
 }
